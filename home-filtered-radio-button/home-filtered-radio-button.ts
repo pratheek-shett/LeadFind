a9505f1e14b5b44ead930160b9e3d72aeb2f4fa7
Pratheek Shetty <pratheekshetty9944@gmail.com>
2026-04-20 21:33:55 +0530

    Add search and filter controls

--- home-filtered-radio-button/home-filtered-radio-button.ts
+++ home-filtered-radio-button/home-filtered-radio-button.ts
@@ -1,25 +1,31 @@
-import { Component,Input,Output,EventEmitter } from '@angular/core';
-import { FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-filtered-radio-button',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home-filtered-radio-button.html',
   styleUrl: './home-filtered-radio-button.css',
 })
 export class HomeFilteredRadioButton {
+  radioValues: string[] = ['All', 'Open', 'Completed', 'In Progress', 'Closed'];
 
-   radioValues : string[] = ["All", "Open", "Completed", "In Progress", "Closed"];
-  
+  @Output()
+  dataFromRadio: EventEmitter<string> = new EventEmitter();
 
-   @Output()
-   dataFromRadio: EventEmitter<string> = new EventEmitter();
-   radioButtonValue: string = "All"
+  sendreloadrequest: EventEmitter<string> = new EventEmitter();
+  radioButtonValue: string = 'All';
 
+  @Input()
+  datagetting: any;
 
-   getValueFromRadio(){
+  getValueFromRadio() {
     this.dataFromRadio.emit(this.radioButtonValue);
-   }
-}
+  }
 
+  reloadtheapifetch() {
+    this.radioButtonValue = 'All';
+    this.dataFromRadio.emit(this.radioButtonValue);
+  }
+}
--- search/search.ts
+++ search/search.ts
@@ -1,6 +1,13 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -8,18 +15,30 @@ import { CommonModule } from '@angular/common';
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
-export class Search {
+export class Search implements AfterViewInit {
   subtitle: string = '';
+  searchTerm: string = '';
 
   @Output()
   storethesearchvalue: EventEmitter<string> = new EventEmitter();
 
-  sendTheSearchElement(){
-   
+  @ViewChild('inputvalue', {})
+  getdata!: ElementRef<HTMLInputElement>;
+
+  @ViewChild('inputvalue')
+  erasevalue!: ElementRef<HTMLInputElement>;
+
+  ngAfterViewInit() {
+    this.getdata.nativeElement.focus();
   }
 
-  getSearchValueFromField(event: HTMLInputElement) {
-    this.subtitle = event.value;
+  getSearchValueFromField() {
+    this.subtitle = this.searchTerm.trim();
     this.storethesearchvalue.emit(this.subtitle);
+  }
+
+  erase() {
+    this.searchTerm = '';
+    this.erasevalue.nativeElement.value = '';
   }
 }
